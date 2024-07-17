ef369f421c1793c93fa92bfb9a4bae1b319121a6
Tony Scott <acsii@tonys-mbp.lan>
2024-07-17 16:45:12 -0400

    Singly linked list traverse method

--- JS_Alg+DS/singlyLinkedList44.js
+++ JS_Alg+DS/singlyLinkedList44.js
@@ -5,7 +5,6 @@ class Node{
         this.next = null;
     }
 }
-
 class SinglyLinkedList{
     constructor(){
         this.head = null;
@@ -25,7 +24,32 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
+    // Pop Method
+
+    // Traverse
+    traverse(){
+        let current = this.head;
+        while(current){
+            if(current){
+                console.log(current.val);
+                current = current.next;
+            }
+        }
+        
+    }
 }
 
 let list = new SinglyLinkedList()
 list.push('Love 4 CS');
+list.push('Data Structures');
+list.push('Algorithms');
+list.push('Merge Sort');
+list.push('Bubble Sort');
+list.push('Algorithms');
+list.push('Linked Lists');
+list.push('Graphs');
+
+
+list.traverse();
+
+
