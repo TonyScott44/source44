7bc5501ad9f3ff9bb41ad0bba0a904e6a2db0007
Tony Scott <goldengate@tonys-imac.lan>
2024-07-24 12:26:52 -0400

    Added notes on Big O Notation

---
+++ JS_Alg+DS/bigO44.js
@@ -0,0 +1,39 @@
+// Big O Notation
+// To analyze the performance of an algorithm, we use Big O Notation.
+
+/* Big O Notation...  
+
+   - provides a numeric representation of the performance of code.
+
+   - provides a high level understanding of the time or space complexity of an algorithm.
+
+   - doesn't care about precision, only about general trends (linear?, quadratic?, constant?)
+
+   It's important to have a precise vocabulary to talk about how code performs.
+
+   When your code slows down or crashes, identifying parts of the code that are inefficient
+   can help us find pain points in our applications.
+
+   The time or space complexity (as measured by Big O) depends only on the algorithm, 
+   not the hardware used to run the algorithm.
+
+*/
+
+
+/* Operations & Assignments to Determine Runtime 
+
+    
+
+   - provides a high level understanding of the time or space complexity of an algorithm.
+
+   - doesn't care about precision, only about general trends (linear?, quadratic?, constant?)
+
+   It's important to have a precise vocabulary to talk about how code performs.
+
+   When your code slows down or crashes, identifying parts of the code that are inefficient
+   can help us find pain points in our applications.
+
+   The time or space complexity (as measured by Big O) depends only on the algorithm, 
+   not the hardware used to run the algorithm.
+   
+*/
